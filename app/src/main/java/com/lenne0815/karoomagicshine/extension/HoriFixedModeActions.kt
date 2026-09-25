f7f8bf0d4881987f5bf29eda50f484b9e4b5fb8e
xrsprint <damian.dorbolo@outlook.com>
2026-09-25 21:40:35 +1000

    feat: add fixed Hori 1300 ride actions

---
+++ app/src/main/java/com/lenne0815/karoomagicshine/extension/HoriFixedModeActions.kt
@@ -0,0 +1,23 @@
+package com.lenne0815.karoomagicshine.extension
+
+import android.content.Context
+import android.content.Intent
+import androidx.glance.GlanceId
+import androidx.glance.action.ActionParameters
+import androidx.glance.appwidget.action.ActionCallback
+
+private abstract class HoriFixedModeAction : ActionCallback {
+    abstract val mode: Hori1300Mode
+    override suspend fun onAction(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
+        context.startService(
+            Intent(context, MagicshineControlService::class.java)
+                .setAction(MagicshineControlService.ACTION_HORI_MODE)
+                .putExtra(MagicshineControlService.EXTRA_HORI_MODE, mode.name),
+        )
+    }
+}
+
+class HoriLowAction : HoriFixedModeAction() { override val mode = Hori1300Mode.LOW }
+class HoriMedAction : HoriFixedModeAction() { override val mode = Hori1300Mode.MED }
+class HoriHighAction : HoriFixedModeAction() { override val mode = Hori1300Mode.HIGH }
+class HoriHighBeamAction : HoriFixedModeAction() { override val mode = Hori1300Mode.HIGH_BEAM }
