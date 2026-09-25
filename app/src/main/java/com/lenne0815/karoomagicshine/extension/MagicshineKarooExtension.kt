f8412d2e2a5e238021620efbf0542de556bb727c
xrsprint <damian.dorbolo@outlook.com>
2026-09-25 21:56:55 +1000

    Bump Karoo extension version to 1.1

--- app/src/main/java/com/lenne0815/karoomagicshine/extension/MagicshineKarooExtension.kt
+++ app/src/main/java/com/lenne0815/karoomagicshine/extension/MagicshineKarooExtension.kt
@@ -15,7 +15,7 @@ import io.hammerhead.karooext.extension.KarooExtension
 import io.hammerhead.karooext.models.ReleaseBluetooth
 import io.hammerhead.karooext.models.RequestBluetooth
 
-class MagicshineKarooExtension : KarooExtension("karoo-magicshine-controls", "1.0") {
+class MagicshineKarooExtension : KarooExtension("karoo-magicshine-controls", "1.1") {
     companion object {
         private const val TAG = "MagicshineExt"
         private const val ACTION_RIDE_APP_OPENED = "io.hammerhead.intent.action.RIDE_APP_OPENED"
