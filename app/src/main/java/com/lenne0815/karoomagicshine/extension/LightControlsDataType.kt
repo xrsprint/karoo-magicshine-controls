3f62b64ff1e5914734b4af84f1a31db68a68f48b
xrsprint <damian.dorbolo@outlook.com>
2026-09-25 21:56:58 +1000

    Register Hori 1300 controls as a new Karoo field type

--- app/src/main/java/com/lenne0815/karoomagicshine/extension/LightControlsDataType.kt
+++ app/src/main/java/com/lenne0815/karoomagicshine/extension/LightControlsDataType.kt
@@ -112,7 +112,7 @@ class LightControlsDataType(extension: String) : DataTypeImpl(extension, TYPE_ID
         }
     }
     companion object {
-        const val TYPE_ID = "DATATYPE_LIGHT_CONTROLS"
+        const val TYPE_ID = "DATATYPE_HORI1300_CONTROLS"
         private const val RENDER_VERSION = 15
 
         private val GREEN_COLOR = Color(0xFF20D39B)
